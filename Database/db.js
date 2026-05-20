b261041d922bad5406016e24c4a944ddf2c52cb1
Lazy Wanwan <ashok@evolveasia.co>
2026-05-20 10:33:10 +0545

    fix env reading

--- Database/db.js
+++ Database/db.js
@@ -1,13 +1,21 @@
+const dotenv = require("dotenv");
 const { Pool } = require("pg");
-require("dotenv").config();
+
+/* ================= ENV ================= */
+const NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = NODE_ENV;
+
+const envFile = NODE_ENV === "production" ? ".env.production" : ".env.local";
+
+dotenv.config({ path: envFile });
+/**========================================= */
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: String(process.env.DB_PASSWORD),
-  port: 5432,
+  port: process.env.DB_PORT || 5432,
 });
-
 
 module.exports = pool;
